614c426f9875e4fd2d35c93facf83493efba9c11
itzmirabel111-ops <itzmirabel111@gmail.com>
2025-09-02 14:14:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// Small enhancements only
+document.getElementById('year').textContent = new Date().getFullYear();
+// Smooth anchor scroll
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', e=>{
+    const href=a.getAttribute('href');
+    if(href.length>1){
+      e.preventDefault();
+      document.querySelector(href).scrollIntoView({behavior:'smooth', block:'start'});
+      history.pushState(null,'',href);
+    }
+  });
+});
